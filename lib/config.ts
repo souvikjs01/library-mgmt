11b3a3ff2176e8f51a8ecad9f59121ecdbf3746d
souvikjs01 <souvik741156@gmail.com>
2025-02-24 14:23:45 +0530

    added prod url

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
     env: {
         apiEndPoint: process.env.NEXT_PUBLIC_API_END_POINT!,
+        prodApiEndPoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
         imagekit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             urlEndPoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL!,
